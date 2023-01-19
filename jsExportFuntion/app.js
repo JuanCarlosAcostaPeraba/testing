8a07ffd78dc743c904fbc242314690410a9930e6
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-01-19 13:24:48 +0000

    feat: :sparkles: export js

---
+++ jsExportFuntion/app.js
@@ -0,0 +1,10 @@
+export function contact(name, age) {
+    return `The name is ${name}. And age is ${age}.`;
+}
+
+export function sum(a, b) {
+    return a + b;
+}
+export function sub(a, b) {
+    return a - b;
+}
